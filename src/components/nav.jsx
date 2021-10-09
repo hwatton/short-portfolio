e26c5ba364e33db8b62d2b7d2bc61c6d38b7f44e
hwatton <66825106+hwatton@users.noreply.github.com>
2021-10-09 21:42:31 +0100

    first commit

---
+++ src/components/nav.jsx
@@ -0,0 +1,11 @@
+function Nav() {
+    return <div id={"navBar"}>
+        <a href="">home</a>
+        <a href="">about</a>
+        <a href="">contact</a>
+        <a href="">projects</a>
+
+    </div>
+}
+
+export default Nav
